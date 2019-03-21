707ae5da9a10078e9fbf9cc76ff4b8380eae5c85
Imran Sayed <imranhsayed@gmail.com>
2019-03-21 14:26:00 +0530

    Add beforeEach and afterEach functions

--- __tests__/ReverseString.test.js
+++ __tests__/ReverseString.test.js
@@ -1,5 +1,13 @@
 const reverseString = require( '../ReverseString' );
 
+// The function called inside of beforeEach will run before every test
+beforeEach( () => initDatabase() );
+// The function called inside of afterEach will run before every test
+afterEach( () => closeDatabase() );
+
+const initDatabase = () => console.log( 'Database initialized' );
+const closeDatabase = () => console.log( 'Database Closed' );
+
 // Check if the function is defined
 test( 'reverseString function exists', () => {
 	expect( reverseString ).toBeDefined();
